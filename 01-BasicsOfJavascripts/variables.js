f761997fcddd82aa8d0d4b884fdd12428baa36de
Manjula-FrontEndDeveloper <manjulamanjula01869@gmail.com>
2025-03-05 17:48:35 +0000

    variables

---
+++ 01-BasicsOfJavascripts/variables.js
@@ -0,0 +1,18 @@
+const accountNum=846345
+var emailId='man@gmail.com'
+let passWord='123'
+myCity='bangalore'
+let accountState
+let accountCity=''
+
+emailId='manjula@ymail.com'
+passWord='2356'
+myCity='yavatmal'
+/*
+avoid using var, because of the issues in block scope and functional scope
+*/
+
+// accountNum=2--> not allowed
+console.log(accountNum);
+console.table([accountNum,emailId,passWord,myCity, accountState, accountCity])
+
