01167c9221b6d78a6a244ea24e2b85b8de91bae9
Manjula-FrontEndDeveloper <manjulamanjula01869@gmail.com>
2025-03-06 16:50:58 +0000

    comparision

---
+++ BasicsJS/comparisions.js
@@ -0,0 +1,20 @@
+console.log(1>2);
+console.log(1==2);
+console.log(2<=1);
+console.log(2!=1);
+console.log(1<2);
+
+console.log('2'>1);
+console.log('02'<1);
+
+console.log(null>0);
+console.log(null==1);
+console.log(null<=1);
+console.log(undefined<=1);
+console.log(undefined>1);
+console.log(undefined==1);
+
+// difference between == and ===
+//== --> compares only value, === --> complares both datatype and value i.e strict check
+
+
